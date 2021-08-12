139aa00b4cc8f452d062c3c53d71db3c14a0e7d6
agustinestrada <agustindeestrada91@gmail.com>
2021-08-12 19:35:44 -0300

    usuarios controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,14 @@ const express = require('express')
 const usersRouter = express.Router()
 const userController = require('../controllers/userController')
 
+// Rutas por GET
 usersRouter.get('/login', userController.login)
-
 usersRouter.get('/register', userController.register)
-
 usersRouter.get('/contact', userController.contacto)
 
+// Rutas por POST
+usersRouter.post('/register',userController.storageUser)
+usersRouter.post('/login',userController.logeo)
 
 
 module.exports = usersRouter;
