bac37b2b9031e3f096eb7f8f7ef180e86c58ab6c
agustinestrada <agustindeestrada91@gmail.com>
2021-09-17 00:48:15 -0300

    validaciones del front hechas

--- public/js/form-validation-create-product.js
+++ public/js/form-validation-create-product.js
@@ -1,104 +1,103 @@
-const { REAL } = require("sequelize/types")
-
 window.addEventListener('load',()=>{
 
     
-//Guardo mi formulario en una variable
-const form = document.querySelector("#form-create")
-const inputName = form.querySelector('#name')
-const inputPrice = form.querySelector('#price')
-const inputCategoria = form.querySelector('#categoria')
-const inputDescripcion = form.querySelector('#descripcion')
-const inputImage = form.querySelector('#producttImage')
+    //Guardo mi formulario en una variable
+    const form = document.querySelector("#form-create")
+    const inputName = form.querySelector('.nombre')
+    const inputPrice = form.querySelector('.precio')
+    const inputCategoria = form.querySelector('.categoria')
+    const inputDescripcion = form.querySelector('.descripcion')
+    const inputImage = form.querySelector('#productImage')
 
 
-const errorName = form.querySelector('.msg-error-name')
-const errorPrice = form.querySelector('.msg-error-precio')
-const errorCategoria = form.querySelector('.msg-error-categoria')
-const errorDescripcion = form.querySelector('.msg-error-descripcion')
-const errorImage = form.querySelector('.msg-error-image')
+    const errorName = form.querySelector('.msg-error-nombre')
+    const errorPrice = form.querySelector('.msg-error-precio')
+    const errorCategoria = form.querySelector('.msg-error-categoria')
+    const errorDescripcion = form.querySelector('.msg-error-descripcion')
+    const errorImage = form.querySelector('.msg-error-image')
 
 
-function isNumeric(n){
-    return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-function resetErrors() {
-    errorName = ""
-    errorPrice = ""
-    errorCategoria = ""
-    errorDescripcion = ""
-}
-
-
-form.addEventListener('submit', (e) => {
-    
-    let hasErrors = []
-    
-    if (inputName.value.length < 3){
-        
-        
-        hasErrors = true
-        
-        errorName.innerHTML = "a Ingrese el nombre del producto"
-        inputName.focus()
-      
-       
+    function isNumeric(n){
+        return !isNaN(parseFloat(n)) && isFinite(n);
     }
- 
 
-    
-    if (!isNumeric(inputPrice.value) || (inputPrice.value < 0)){
-        
-        errorPrice.innerHTML = "a Ingrese un valor mayor a 0"
+    let hasErrors = false
 
-        if(!hasErrors){
+    form.addEventListener('submit', (e) => {
+
+        console.log(inputImage)
+            
+            
+        if (inputName.value.length < 3){
+            
+            
+            hasErrors = true
+            
+            errorName.innerHTML = "a Ingrese el nombre del producto"
+            inputName.focus()
         
-            inputPrice.focus()
+        
+        }
+
+            
+        if (inputPrice.value <= 0 || inputPrice == null || inputPrice == undefined ){
+                
+                errorPrice.innerHTML = "a Ingrese un valor mayor a 0"
+
+                if(!hasErrors){
+                
+                    inputPrice.focus()
+                    
+                }
+                hasErrors = true
+            }
+        
+            
+        if (!inputCategoria.value.length > 0){
+                
+                errorCategoria.innerHTML = "a Por favor ingrese la categoria"
+                
+                if(!hasErrors){
+                
+                    inputCategoria.focus()
+                    
+                }
+                hasErrors = true
+
+            }
+
+        if (inputDescripcion.value.length <= 0 || inputDescripcion.value.length > 4000){
+            
+            errorDescripcion.innerHTML = "a Debe ingresar la descripcion del producto. Con un maximo de 4000 caracateres"
+
+            if(!hasErrors){
+            
+                inputDescripcion.focus()
+                
+            }
+
+            hasErrors = true
             
         }
-        hasErrors = true
-    }
 
-    
-    if (!inputCategoria.value.length > 0){
-        
-        errorCategoria.innerHTML = "Por favor ingrese la categoria"
-        if(!hasErrors){
-        
-            inputCategoria.focus()
+
+        if (!inputImage.value){
+
+            errorImage.innerHTML = "Debe seleccionar una imagen"
+            if(!hasErrors){
             
+                inputImage.focus()
+                
+            }
+            hasErrors = true
+        
         }
-        hasErrors = true
 
-    }
-    if (inputDescripcion.value.length < 0 || inputDescripcion.length > 4000){
-        
-        errorDescripcion.innerHTML = "Debe ingresar la descripcion del producto. Con un maximo de 4000 caracateres"
-
-        if(!hasErrors){
-        
-            inputDescripcion.focus()
-            
+        if (hasErrors){
+            e.preventDefault()
         }
-        hasErrors = true
-        
-    }
-    if (!inputImage.value){
 
-        errorDescripcion.innerHTML = "Debe seleccionar una imagen"
-        if(!hasErrors){
-        
-            inputImage.focus()
-            
-        }
-        hasErrors = true
-    }
-        
-    if (hasErrors){
-    e.preventDefault()
-    }
-    
-})
+
+    })
 
 })
